92dc8279d66a14a28fc494c0e9bc26a87aa5c19d
v200123 <295799682@qq.com>
2024-05-16 23:31:14 +0800

    修改查询订单

--- src/main/java/cn/fishei/competition/controller/OrderController.java
+++ src/main/java/cn/fishei/competition/controller/OrderController.java
@@ -8,6 +8,7 @@ import cn.fishei.competition.bean.R;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Consumer;
 
 /**
  * 订单控制器
@@ -35,7 +37,7 @@ public class OrderController {
      * @param order
      * @return
      */
-    @RequestMapping("/create")
+    @PostMapping("/create")
     public R create(@RequestBody Order order){
         // System.out.println("order:"+order);
         order.setOrder_time(new Date());
@@ -56,12 +58,19 @@ public class OrderController {
      */
     @RequestMapping("/get")
     public R get(String table_number,String transac_status){
-        Order order=orderService.list(new QueryWrapper<Order>().eq("table_number", table_number).eq("transac_status", transac_status).orderByDesc("order_time")).get(0);
+        List<Order> list = orderService.list();
+        list.forEach(new Consumer<Order>() {
+            @Override
+            public void accept(Order order) {
+                List<OrderDetail> goods_list = orderDetailService.list(new QueryWrapper<OrderDetail>().eq("m_id", order.getId()));
+                order.setGoods_list(goods_list);
+            }
+        });
+        Order order=orderService.list().get(0);
         System.out.println("11:"+order);
-        List<OrderDetail> goods_list = orderDetailService.list(new QueryWrapper<OrderDetail>().eq("m_id", order.getId()));
         Map<String,Object> map=new HashMap<>();
-        map.put("goods_list",goods_list);
-        map.put("menu",order);
+        map.put("orderInfo",list);
+//        map.put("menu",order);
         return R.ok(map);
     }
 
